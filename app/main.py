ae695655f602a8a1d6fb8315729a1f892a9c5300
Samuel Jr. Berkoh <iamranchojr@gmail.com>
2024-11-26 09:56:26 +0000

    chore: configurations for deployment to heroku (#1)

--- app/main.py
+++ app/main.py
@@ -1,8 +1,39 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
+from app.config import settings
 from app.router import router
 
 app = FastAPI(title='Hustlesasa Recommendation Engine')
+
+cors_allowed_origins = [
+    'http://localhost',
+    'http://localhost:7777',
+    'https://hustlesasa-recommend-engine-699e1f9da4f3.herokuapp.com',
+]
+
+
+# apply cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=cors_allowed_origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+if settings.SECURE_SSL_REDIRECT:
+    # apply redirect to https middleware
+    app.add_middleware(HTTPSRedirectMiddleware)
+
+    app.add_middleware(
+        TrustedHostMiddleware,
+        allowed_hosts=[
+            'hustlesasa-recommend-engine-699e1f9da4f3.herokuapp.com',
+        ]
+    )
 
 
 @app.get('/')
